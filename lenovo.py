63d2a134b4842c25f2dba8880986cb3dfe74fd2b
phuang00 <phuang00@stuy.edu>
2019-09-10 11:54:24 -0400

    debugging code to get random team name

--- lenovo.py
+++ lenovo.py
@@ -5,6 +5,6 @@ KREWES = {'orpheus':['Emily', 'Kevin'], 'rex':['William', 'Joseph']}
 def function(dict):
     team = dict.keys()[rand.randint(0,len(dict) - 1)]
     print("team" + team)
-    print(dict[team][rand.randint(0,len(team) - 1)])
+    #print(dict[team][rand.randint(0,len(team) - 1)])
 
 function(KREWES)
