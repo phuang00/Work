1695ad3b905563e53b54b4a67a7234b6b9510647
phuang00 <phuang00@stuy.edu>
2019-09-10 12:48:45 -0400

    fixed issue with dict.key() in python3 and found typo in print statement

--- lenovo.py
+++ lenovo.py
@@ -3,8 +3,10 @@ import random as rand
 KREWES = {'orpheus':['Emily', 'Kevin'], 'rex':['William', 'Joseph']}
 
 def function(dict):
-    team = dict.keys()[rand.randint(0,len(dict) - 1)]
-    print("team" + team)
-    #print(dict[team][rand.randint(0,len(team) - 1)])
+    teams = list()
+    for a in dict.keys():
+        teams.append(a)
+    team = teams[rand.randint(0,len(dict) - 1)]
+    print(dict[team][rand.randint(0,len(dict[team]) - 1)])
 
 function(KREWES)
