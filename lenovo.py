88876c87b8c1b876a2f2fc71b07594b734a5b1f0
phuang00 <phuang00@stuy.edu>
2019-09-10 09:22:41 -0400

    add python file for the task from yesterday

---
+++ lenovo.py
@@ -0,0 +1,10 @@
+import random as rand
+
+KREWES = {'orpheus':['Emily', 'Kevin'], 'rex':['William', 'Joseph']}
+
+def function(dict):
+    team = dict.keys()[rand.randint(0,len(dict) - 1)]
+    print("team" + team)
+    print(dict[team][rand.randint(0,len(team) - 1)])
+
+function(KREWES)
