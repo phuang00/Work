b4b5b2c8f26e2cf2cdb7b568c0775f556d6002f4
phuang00 <phuang00@stuy.edu>
2019-09-10 15:05:18 -0400

    added comments in

--- lenovo.py
+++ lenovo.py
@@ -4,13 +4,17 @@ KREWES = {'orpheus':['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffa
           'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph',
           'Tanzim', 'David', 'Yevgeniy'],
           'rex':['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice',
-          'Hilary', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+          'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
           'endymion':['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth',
           'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']}
 
 def function(dict):
     teams = list(dict.keys())
+    ## stores the keys of the dictionary as teams
     team = rand.choice(teams)
+    ## credit: the function rand.choice was suggested by Kiran when I was discussing randint with her
+    ## choose a random team from the list of teams
     print(rand.choice(dict[team]))
+    ## choose a random name from the randomly chosen team and print it
 
 function(KREWES)
