3a24262b0917e5fec5a6fc006292a30c169da1f0
phuang00 <phuang00@stuy.edu>
2019-09-20 16:42:01 -0400

    wrote code converting csv file to dict

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,8 +1,9 @@
 #Peihua Huang
 #SoftDev1 pd2
-#K -- Static folder and templates
+#K10 -- Occupations Flask app
 #2019-09-19
 
+import csv, random
 from flask import Flask, render_template
 app = Flask(__name__) #create instance of class Flask
 
@@ -11,11 +12,14 @@ def hello_world():
     print(__name__) #where will this go?
     return "No hablo queso!"
 
-coll = [0, 1, 1, 2, 3, 5, 8]
-
 @app.route("/occupyflaskst")
 def template():
-    return render_template()
+    dict = {}
+    with open('data/occupations.csv') as file:
+        csv_reader = csv.reader(file, delimiter = ',')
+        for row in csv_reader:
+            dict[row[0]] = row[1]
+    return render_template('template.html', title = "Occupation Data")
 
 
 if __name__ == "__main__":
