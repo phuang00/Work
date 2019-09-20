c99cf4c068a2ad8f14aa7c9380f7573464b27d1e
phuang00 <phuang00@stuy.edu>
2019-09-20 13:09:33 -0400

    started hw

---
+++ 10_occ/app.py
@@ -0,0 +1,23 @@
+#Peihua Huang
+#SoftDev1 pd2
+#K -- Static folder and templates
+#2019-09-19
+
+from flask import Flask, render_template
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/") #assign following fxn to run when root route requested
+def hello_world():
+    print(__name__) #where will this go?
+    return "No hablo queso!"
+
+coll = [0, 1, 1, 2, 3, 5, 8]
+
+@app.route("/occupyflaskst")
+def template():
+    return render_template()
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
