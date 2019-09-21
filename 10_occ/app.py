d4f2417d0992c926f61df81999e9b0c6a55f6bdc
phuang00 <phuang00@stuy.edu>
2019-09-21 15:31:32 -0400

    added comments to code

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -4,26 +4,33 @@
 #2019-09-19
 
 import csv, random
+# import modules
 from flask import Flask, render_template
-app = Flask(__name__) #create instance of class Flask
+app = Flask(__name__)
+#create instance of class Flask
 
-@app.route("/") #assign following fxn to run when root route requested
+@app.route("/")
 def hello_world():
-    print(__name__) #where will this go?
+    print(__name__)
     return "Please go to /occupyflaskst"
 
 @app.route("/occupyflaskst")
 def template():
     dict = {}
     with open('data/occupations.csv') as file:
+    # read in file
         csv_reader = csv.reader(file, delimiter = ',')
+        # split each line based on commas
         next(csv_reader, None)
+        # skip the first line, so heading is not added to dictionary
         for row in csv_reader:
             dict[row[0]] = float(row[1])
+            # for every row in the file, add job and percentage to dictionary
     del(dict["Total"])
-    job = random.choices(list(dict.keys()), list(dict.values()))
+    # delete the last row that sums up the percentage
+    job = random.choices(list(dict.keys()), list(dict.values())) # choose a job randomly based on weighted percentages
     return render_template('template.html', title = "Occupation Data", heading = "Occupations and their Statistics", jobs = "Job Class", percent = "Percentage", team = " Team Cereal Before Milk: Peihua Huang, David Lupea", dict = dict.items(), rand_job = job[0])
-
+    # render template and insert variables in accordingly
 
 if __name__ == "__main__":
     app.debug = True
