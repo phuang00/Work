edf33f0e02e96cfcf5548f900d693a8517f6e18b
phuang00 <phuang00@stuy.edu>
2019-10-02 11:30:34 -0400

    fixed issue with sessions

--- 15_login/app.py
+++ 15_login/app.py
@@ -9,25 +9,27 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import session
+import os
 # imported Flask, render_template, and request
 app = Flask(__name__)
 #create instance of class Flask
 
 usr = "apples"
 pwd = "bananas"
-#session["loggedIn"] = False
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 def root():
-    #if (session["loggedIn"]):
-        #return redirect(url_for("welcome"))
-    return render_template("template.html")
+    if ("loggedIn" in session):
+        return redirect(url_for("welcome"))
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/auth")
 def authenticate():
     if (usr == request.args['username']):
         if (pwd == request.args['password']):
-            #session["loggedIn"] = True
+            session["loggedIn"] = True
             return redirect(url_for("welcome"))
         else:
             return render_template("error.html", rsn = "incorrect password")
@@ -39,6 +41,15 @@ def authenticate():
 def welcome():
     return render_template("welcome.html")
 
+@app.route("/login")
+def login():
+    return render_template("template.html")
+
+@app.route("/logout")
+def logout():
+    if ("loggedIn" in session):
+        session.pop("loggedIn")
+    return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
