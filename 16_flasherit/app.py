c9ded45a2cea519fe54d46edafefd7c9666ebe45
phuang00 <phuang00@stuy.edu>
2019-10-03 10:18:53 -0400

    added comments

--- 16_flasherit/app.py
+++ 16_flasherit/app.py
@@ -1,6 +1,6 @@
 #Peihua Huang
 #SoftDev1 pd2
-#K16 -- Flashing
+#K16 -- Flashing and Template Inheritance
 #2019-10-03
 
 from flask import Flask
@@ -44,26 +44,36 @@ def authenticate():
         else:
             # if password is incorrect
             return render_template("error.html", rsn = "incorrect password")
-            # render error template 
+            # render error page and error message
     else:
+        # if username is incorrect
         return render_template("error.html", rsn = "incorrect username")
+        # render error page and error message
     return render_template("error.html", rsn = "bad juju")
+    #if it fails for any othe reason, return error page and error message
 
 @app.route("/welcome")
 def welcome():
     if ("loggedIn" in session):
+        # is user is logged in
         return render_template("welcome.html")
+        # redirect to welcome page
     return redirect(url_for("login"))
+    # else redirect to login page
 
 @app.route("/login")
 def login():
     return render_template("template.html")
+    # render template
 
 @app.route("/logout")
 def logout():
     if ("loggedIn" in session):
+    # if user is logged in
         session.pop("loggedIn")
+        # pop session to log user out
     return redirect("/")
+    #redirect user back to root route
 
 if __name__ == "__main__":
     app.debug = True
