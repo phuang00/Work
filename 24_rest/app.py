a6c94d6f3aabac61bf7a5bdf0b49901635555cb9
phuang00 <phuang00@stuy.edu>
2019-11-12 15:08:51 -0500

    got the html page to load

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,17 +1,17 @@
-#Benjamin Peihua Huang
+#Benjamin Avrahami, Peihua Huang (Team PI)
 #SoftDev1 pd2
 #K24 -- A RESTful Journey Skyward
 #2019-11-12
 
 from flask import Flask
 from flask import render_template
-import urllib3, json
+import urllib, json
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib3.PoolManager()
-    response = u.request('GET', "https://api.nasa.gov/planetary/apod?api_key=KaSPKF6wwTVHdhAzFcp9iivZUrgmlz1TtxRHge2U")
+    u = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=KaSPKF6wwTVHdhAzFcp9iivZUrgmlz1TtxRHge2U")
+    response = u.read()
     data = json.loads(response)
     return render_template("index.html", pic=data['url'])
 
