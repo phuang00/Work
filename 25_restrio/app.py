42a58e47ba3021c47aff73df9269bfcc0841021e
phuang00 <phuang00@stuy.edu>
2019-11-13 09:30:31 -0500

    added two APIs

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -3,9 +3,9 @@
 #K25 -- Getting More REST
 #2019-11-12
 
+import urllib, json
 from flask import Flask
 from flask import render_template
-import urllib, json
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,7 +13,14 @@ def root():
     u = urllib.request.urlopen("https://ghibliapi.herokuapp.com/locations")
     response = u.read()
     data = json.loads(response)
-    return render_template("index.html", locations=data['name'])
+    return render_template("index.html", locations=data)
+
+@app.route("/ddclasses")
+def cards():
+    u = urllib.request.urlopen("http://www.dnd5eapi.co/api/classes")
+    response = u.read()
+    data = json.loads(response)
+    return render_template("results.html", classes=data["results"])
 
 if __name__ == "__main__":
     app.debug = True
