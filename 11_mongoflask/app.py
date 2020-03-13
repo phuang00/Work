5977d3d8242d743009e654ca3ffb015a63fdd362
phuang00 <phuang00@stuy.edu>
2020-03-12 22:17:46 -0400

    test

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -5,7 +5,7 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session
 import events
-import senators
+import senator
 
 app = Flask(__name__)
 
@@ -16,16 +16,36 @@ def home():
 @app.route("/history")
 def history():
     if "place" in request.args:
-        print("hi")
-        data = events.get_by_place("Egypt")
-        for item in data:
-            print(item['description'])
+        # print("hi")
+        # data = events.get_by_place("Egypt")
+        # for item in data:
+            # print(item['description'])
         return render_template("history.html", event=events.get_by_place(request.args["place"]))
     return render_template("history.html")
 
 @app.route("/senators")
 def senators():
-    return render_template("senators.html")
+    state = ""
+    party = ""
+    gender = ""
+    first = ""
+    last = ""
+    num_gen = ""
+    if "state" in request.args:
+        state = senator.find_state(request.args["state"])
+    if "party" in request.args:
+        party = senator.find_party(request.args["party"])
+    if "gender" in request.args:
+        gender = senator.find_gender(request.args["gender"])
+    if "first" in request.args:
+        first = senator.find_website(request.args["first"])
+    if "last" in request.args:
+        last = senator.find_description(request.args["last"])
+    if "num_gen" in request.args:
+        num_gen = senator.find_num_gender(request.args["num_gen"], request.args["num"])
+    return render_template("senators.html", state=state, party=party,
+                                            gender=gender, first=first,
+                                            last=last, num_gen=num_gen)
 
 if __name__ == "__main__":
     app.debug = True
